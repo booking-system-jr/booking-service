7a054e436cc92e63a7d59cfa7e572b5914b4276b
Yaroslava Nechaieva <shdwrazedev@gmail.com>
2026-02-04 23:07:39 +0100

    init

---
+++ src/main/java/io/github/sever0x/bookingservice/BookingServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.sever0x.bookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServiceApplication.class, args);
+	}
+
+}
